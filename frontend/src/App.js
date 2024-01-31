088fee77d5d21d85825c31bbfb0531fdd94c74de
Natan Balthazar <balthazarnatan@gmail.com>
2024-01-31 01:34:43 -0300

    time2542

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,23 @@
+import React, { useState } from 'react';
+import Header from './Header';
+
 function App() {
+  const [contador, setContador] = useState(0);
+
+  function increment() {
+    setContador(contador + 1);
+  }
+
+  function clear() {
+    setContador(0);
+  }
+
   return (
-    <h1> Hello World</h1>
+    <div>
+      <Header>Contador: {contador}</Header>
+      <button onClick={increment}>Incrementar</button>
+      <button onClick={clear}>Limpar</button>
+    </div>
   );
 }
 
